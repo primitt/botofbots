a9e4399c9138f4dbc1522a0fe09a9c39e3782238
primboi <81822069+primitt@users.noreply.github.com>
2021-09-22 11:27:29 -0700

    e

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 from datetime import datetime
 from dotenv import load_dotenv
 import os
-import random
+import random as r
 import json
 TOKEN = os.getenv('TOKEN')
 client = discord.Client()
@@ -22,13 +22,23 @@ async def on_ready():
         if message.content.startswith("svms"):
             command = message.content.strip("svms ").split(" ")
     
-            if command[0].lower() == "datetime":    
+            if command[0].lower() == "datetime": #current date time 
                 now = datetime.now()
                 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
                 await message.channel.send(dt_string)
 
-            if command[0].lower() == "random"
-                random.randint("")
+            if command[0].lower() == "random": #random number gen
+                try:
+                    randomnum = r.randint(command[1])
+                    numint = "The random number you chose from the numbers: ", command[1], "are", randomnumber
+                    await message.channel.send()
+                else:
+                    await message.channel.send("Sorry, please format your response like this: "
+                    + "\n <number>, <number>")
+            #end of code
+
+
+                
 
 client.run(TOKEN)
 
