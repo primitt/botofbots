53f4f99c50e314c26129f9250c88e0833d994479
primboi <81822069+primitt@users.noreply.github.com>
2021-09-22 11:16:35 -0700

    e

--- main.py
+++ main.py
@@ -2,28 +2,31 @@ import discord
 import requests
 import time
 from datetime import datetime
-client.run()
+from dotenv import load_dotenv
+import os
+TOKEN = os.getenv('TOKEN')
+client = discord.Client()
 
 # datetime object containing current date and time
 @client.event
 async def on_ready():
     print("Logged in as {0.user}".format(client))
-    await client.change_presence(
-        activity=discord.Game(
-            name="with svms" + PREFIX + "help"))
+
     @client.event
     async def on_message(message):
         if message.author == client.user:
             return
     
         if message.content.startswith("svms"):
-            command = message.content.strip("svko ").split(" ")
+            command = message.content.strip("svms ").split(" ")
     
             if command[0].lower() == "datetime":    
                 now = datetime.now()
                 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
                 await message.channel.send(dt_string)
 
-client.run("ODkwMTA0MjkxMTcwMzUzMTYy.YUq8OA.tcs2C5u6GUstQHOwYuJe9XAuAEs")
+client.run("ODkwMTA0MjkxMTcwMzUzMTYy.YUq8OA.dr1roOR52mUccsSzHCuZKS_JlyY")
+
+
 
 
